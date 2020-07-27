d79a76d3ecde98ba7333b747c288ff9ee261393d
WebKawsar <web.kawsarahmed@gmail.com>
2020-07-27 21:42:49 +0600

    updated

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
-/******************************************Incomplete ***********************************/
+/******************************************complete ***********************************/
 
-var screen = document.getElementById("screen");
+var history = document.getElementById("history");
+var sign = document.getElementById("sign");
+var result = document.getElementById("result");
 var buttons = document.querySelectorAll("button");
+
+let resultValue = "";
 
 for (let i = 0; i < buttons.length; i++) {
 
@@ -10,12 +14,111 @@ for (let i = 0; i < buttons.length; i++) {
     
     element.addEventListener("click", function(){
         
-        buttonText = event.formTarget.innerText;
+        var history = document.getElementById("history");
+        var sign = document.getElementById("sign");
+        var result = document.getElementById("result");
+        
+        buttonText = event.target.innerText;
 
-        console.log(buttonText);
+        if(buttonText === "AC") {
+
+            
+            history.innerText = "";
+            sign.innerText = "";
+            result.innerText = "";
+            resultValue = "";
+
+        }
+        else if(buttonText == 0 || buttonText == 1 || buttonText == 2 || buttonText == 3 || buttonText == 4 || buttonText == 5 || buttonText == 6 || buttonText == 7 || buttonText == 8 || buttonText == 9){
+
+            resultValue = resultValue + buttonText;
+            result.innerText = resultValue;
+            
+                // getResultValue = document.getElementById("result").innerText;
+                // setValue = getResultValue + buttonText;
+                // document.getElementById("result").innerText = setValue;
+
+        }
+        else if(buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/"){
+
+                // historyValue = resultValue + " " + buttonText;
+                // history.innerText = historyValue;
+                // result.innerText = "";
+                // resultValue = "";
+
+                
+                history.innerText = resultValue;
+                sign.innerText = buttonText;
+                result.innerText = "";
+                resultValue = "";
+                
+                
+        }
+        else if(buttonText == "=") {
+
+
+                if(sign.innerText == "+"){
+                    
+
+                    historyValue = parseFloat(history.innerText);
+                    resultValue =  parseFloat(result.innerText);
+                    getValue = eval(historyValue + resultValue);
+                    sign.innerText = getValue;
+
+                    result.innerText = "";
+                    history.innerText = "";
+                    resultValue = "";
+
+
+                }
+                if(sign.innerText == "-"){
+
+                    historyValue = parseFloat(history.innerText);
+                    resultValue =  parseFloat(result.innerText);
+                    getValue = eval(historyValue - resultValue);
+                    sign.innerText = getValue;
+
+                    result.innerText = "";
+                    history.innerText = "";
+                    resultValue = "";
+
+                }
+
+                if(sign.innerText == "*"){
+                    
+
+                    historyValue = parseFloat(history.innerText);
+                    resultValue =  parseFloat(result.innerText);
+                    getValue = eval(historyValue * resultValue);
+                    sign.innerText = getValue;
+
+                    result.innerText = "";
+                    history.innerText = "";
+                    resultValue = "";
+
+                }
+
+                if(sign.innerText == "/"){
+
+                    historyValue = parseFloat(history.innerText);
+                    resultValue =  parseFloat(result.innerText);
+                    getValue = eval(historyValue / resultValue);
+                    sign.innerText = getValue;
+
+                    result.innerText = "";
+                    history.innerText = "";
+                    resultValue = "";
+
+                }
+
+                
 
 
 
+        }
+        
+
+        
     });
 
     
